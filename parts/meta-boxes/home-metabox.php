3f69e9f59fa71340ca9cf136163ff9d46da6ba97
Federico <admin@imgdigital.com.ar>
2016-06-07 19:16:35 -0300

    Show Hide Home preferences

--- parts/meta-boxes/home-metabox.php
+++ parts/meta-boxes/home-metabox.php
@@ -5,7 +5,6 @@ Post Type: page,post,events,tvvideo,faculty,classes
 Description: Options to use the post at Home Page
 Priority: high
 Order: 3
-Collapse: true
 Context: side
 */
 
@@ -14,7 +13,7 @@ piklist('field',
         'type' => 'radio'
     , 'scope' => 'post_meta' // Not used for settings sections
     , 'field' => 'imgd_destacado'
-    , 'value' => '0' // Sets default to Option 2
+    , 'value' => 'no' // Sets default to Option 2
     , 'label' => __('Home Page', 'imgd')
     , 'help' => __('Publish this post on the home Page', 'imgd')
     , 'attributes' => array(
@@ -22,53 +21,73 @@ piklist('field',
     )
     , 'position' => 'start'
     , 'choices' => array(
-        '0' => __('No','imgd')
-    , '1' => __('Yes','imgd')
+        'yes' => __('Yes', 'imgd')
+        , 'no' => __('No', 'imgd')
     )
     ));
 
 piklist('field', array(
     'type' => 'number'
-,'field' => 'imgd_destacado_order'
-,'value' => 1
-,'label' => __('Post Order', 'imgd')
-,'help' => __('Set the order where the post must be showed in the home page, the number must be 1 to 5 ', 'imgd')
-,'attributes' => array(
-        'class' => 'small-text'
-    )
+    , 'scope' => 'post_meta'
+    , 'field' => 'imgd_destacado_order'
+    , 'value' => 1
+    , 'label' => __('Post Order', 'imgd')
+    , 'help' => __('Set the order where the post must be showed in the home page, the number must be 1 to 5 ', 'imgd')
+    , 'conditions' => array(
+            array(
+                'field' => 'imgd_destacado'
+                , 'value' => 'yes'
+            )
+        )
+    , 'attributes' => array(
+            'class' => 'small-text'
+        )
 ));
 
 piklist('field', array(
     'type' => 'text'
-,'field' => 'imgd_destacado_title'
-,'value' => ''
-,'label' => __('Promotional Title (optional)', 'imgd')
-,'help' => __('This is optional, and will be used as the title in the Home Page, if is empty, the Home will use the Post Title', 'imgd')
-,'attributes' => array(
-        'class' => 'regular-text'
-    )
+    , 'scope' => 'post_meta'
+    , 'field' => 'imgd_destacado_title'
+    , 'value' => ''
+    , 'label' => __('Promotional Title (optional)', 'imgd')
+    , 'help' => __('This is optional, and will be used as the title in the Home Page, if is empty, the Home will use the Post Title', 'imgd')
+    , 'conditions' => array(
+            array(
+                'field' => 'imgd_destacado'
+                ,'value' => 'yes'
+            )
+        )
+    , 'attributes' => array(
+            'class' => 'regular-text'
+        )
 ));
 
 piklist('field', array(
     'type' => 'file'
-    ,'field' => 'imgd_destacado_image' // Use these field to match WordPress featured images.
-    ,'scope' => 'post_meta'
-    ,'label' => __('Promotional Image (optional)', 'imgd')
-,'help' => __('Set the promotional Image to be used in the Home Page insted of the Feature Image.', 'imgd')
-,'options' => array(
-            'title' => __('Image to be showed at Home Page', 'imgd')
-            ,'modal_title' => __('Add Image', 'imgd')
-            ,'button' => __('Add Image', 'imgd')
-            ,'basic'=>true
-        ),
-        'validate' => array(
+    , 'field' => 'imgd_destacado_image' // Use these field to match WordPress featured images.
+    , 'scope' => 'post_meta'
+    , 'label' => __('Promotional Image (optional)', 'imgd')
+    , 'help' => __('Set the promotional Image to be used in the Home Page insted of the Feature Image.', 'imgd')
+    , 'conditions' => array(
             array(
-                'type' => 'limit'
-                ,'options' => array(
-                    'min' => 0
-                    ,'max' => 1
-                )
-            ,'message' => __('Limit 1 image', 'imgd')
+                'field' => 'imgd_destacado'
+                ,'value' => 'yes'
             )
         )
+    , 'options' => array(
+            'title' => __('Image to be showed at Home Page', 'imgd')
+        , 'modal_title' => __('Add Image', 'imgd')
+        , 'button' => __('Add Image', 'imgd')
+        , 'basic' => true
+        ),
+    'validate' => array(
+        array(
+            'type' => 'limit'
+            ,'options' => array(
+                'min' => 0
+                ,'max' => 1
+            )
+        , 'message' => __('Limit 1 image', 'imgd')
+        )
+    )
 ));
